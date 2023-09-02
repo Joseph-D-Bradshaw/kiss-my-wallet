8a124bc6f8f592a9b4270532eebdde7b97eb5f80
= <joseph.bradshaw@outlook.com>
2023-09-03 01:08:16 +0200

    added endpoints skeleton in express

--- index.ts
+++ index.ts
@@ -1,13 +1,48 @@
-import express, { Request, Response } from "express"
+import express from "express"
+import { createDataBase } from "./database/database"
 
+
+const db = createDataBase()
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT ?? 8000
+
+app.use(express.static('public'))  // for hosting some arbitrary html/js for testing API easily
+app.use(express.json())  // for parsing JSON bodies
+
 
 app.get('/', (req, res) => {
-    res.send('Hello there')
+    res.sendFile('./public/index.html')
+})
+
+app.get('/users', (req, res) => {
+    res.send('You hit the /users GET endpoint')
+})
+
+app.get('/categories', (req, res) => {
+    res.send('You hit the /categories GET endpoint')
+})
+
+app.get('/transactions', (req, res) => {
+    res.send('You hit the /transactions GET endpoint')
+})
+
+app.post('/users', (req, res) => {
+    res.send('You hit the /users POST endpoint')
+})
+
+app.post('/categories', (req, res) => {
+    res.send('You hit the /categories POST endpoint')
+})
+
+app.post('/transactions', (req, res) => {
+    res.send('You hit the /transactions POST endpoint')
 })
 
 
-app.listen(port, () => {
-    console.log(`server is running on port http://localhost:${port}`)
-})
+if (port) {
+    app.listen(port, () => {
+        console.log(`server is running on port http://localhost:${port}`)
+    })
+} else {
+    console.error("Server not started, port not acquired")
+}
